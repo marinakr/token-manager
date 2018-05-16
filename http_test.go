505f75cc04c35ec02ac16e0c8189c1f94efb3cec
Marina <marinakrasnovatr81@gmail.com>
2018-05-16 22:32:40 +0000

    cover reg-email (first part) by tests

--- http_test.go
+++ http_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -42,6 +43,101 @@ func TestRegCheckBadJson(t *testing.T) {
 		} else {
 			if resp.StatusCode != http.StatusBadRequest {
 				t.Fatal("Should fails as bad request")
+			}
+			defer resp.Body.Close()
+		}
+	}
+}
+
+func TestRegCheckBadEmail(t *testing.T) {
+	var jsonStr = []byte(`{"email":";X@@example", "nick":"any"}`)
+	req, err := http.NewRequest("POST", "http://localhost:2803/reg-email", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		} else {
+			if resp.StatusCode != http.StatusBadRequest {
+				t.Fatal("Should fails as bad request")
+			}
+			defer resp.Body.Close()
+		}
+	}
+}
+
+func TestRegCheckBadNickName(t *testing.T) {
+	var jsonStr = []byte(`{"email":"johndoe@any.lynx", "nick":"john@"}`)
+	req, err := http.NewRequest("POST", "http://localhost:2803/reg-email", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		} else {
+			if resp.StatusCode != http.StatusBadRequest {
+				t.Fatal("Should fails as bad request")
+			}
+			defer resp.Body.Close()
+		}
+	}
+}
+
+func TestRegCheckShouldPass(t *testing.T) {
+	var jsonStr = []byte(`{"email":"johndoe@any.lynx", "nick":"john"}`)
+	req, err := http.NewRequest("POST", "http://localhost:2803/reg-email", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		} else {
+			if resp.StatusCode != http.StatusOK {
+				t.Fatal("Should pass:")
+			}
+			defer resp.Body.Close()
+		}
+	}
+}
+
+func TestRegCheckEmailConflict(t *testing.T) {
+	var jsonStr = []byte(`{"email":"johndoe@any.lynx", "nick":"any"}`)
+	req, err := http.NewRequest("POST", "http://localhost:2803/reg-email", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		} else {
+			if resp.StatusCode != http.StatusConflict {
+				t.Fatal("Should fails email conflict")
+			}
+			defer resp.Body.Close()
+		}
+	}
+}
+
+func TestRegCheckNickConflict(t *testing.T) {
+	var jsonStr = []byte(`{"email":"any-johndoe@any.lynx", "nick":"john"}`)
+	req, err := http.NewRequest("POST", "http://localhost:2803/reg-email", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		} else {
+			if resp.StatusCode != http.StatusConflict {
+				t.Fatal("Should fails email conflict")
 			}
 			defer resp.Body.Close()
 		}
